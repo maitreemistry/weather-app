d848375741896fd3e40ff2a670db64476eb3c3f7
Maitree Mistry <162182059+maitreemistry@users.noreply.github.com>
2025-07-05 20:49:16 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 const url =
 	'https://api.openweathermap.org/data/2.5/weather';
 const apiKey =
-	'f00c38e0279b7bc85480c3fe775d518c';
+	'api_key';
 
 $(document).ready(function () {
 	weatherFn('Pune');
